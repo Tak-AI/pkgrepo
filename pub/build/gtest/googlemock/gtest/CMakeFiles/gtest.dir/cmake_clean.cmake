968272632ece43a3b52c2dd214e7b43f6e5d2451
Seiichi Takai <seiichi.takai.q7@dc.tohoku.ac.jp>
2021-06-10 13:49:03 +0900

    first commit

---
+++ pub/build/gtest/googlemock/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/hklab6380/ros_ws/pub/lib/libgtest.pdb"
+  "/home/hklab6380/ros_ws/pub/lib/libgtest.so"
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
