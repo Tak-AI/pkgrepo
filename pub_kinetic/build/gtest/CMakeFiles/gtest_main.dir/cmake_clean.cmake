850dd055d31beaf8b11b5563809b558f42a71084
yoshiscythe <yoshiscythe@gmail.com>
2021-06-10 18:58:04 +0900

    commit

---
+++ pub_kinetic/build/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/ayg/ros_ws/pkgrepo/pub_kinetic/lib/libgtest_main.pdb"
+  "/home/ayg/ros_ws/pkgrepo/pub_kinetic/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
